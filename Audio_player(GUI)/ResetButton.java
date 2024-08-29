fde910a3b734bec3c6bf83253eef93dd94376a4e
Kacper Kowalski <kkowalski0477@gmail.com>
2024-08-29 11:37:53 +0200

    Reset button
    A button responsible for reseting the whole song and starting it over

---
+++ Audio_player(GUI)/ResetButton.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ResetButton extends JButton{
+    ResetButton(){
+
+        this.setText("RESET");
+        this.setForeground(Color.GREEN);
+        this.setBackground(Color.BLACK);
+        this.setBorderPainted(false);
+
+    }
+}
