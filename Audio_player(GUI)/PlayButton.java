796c629db753c7484fb7367c77dafb8a01f08897
Kacper Kowalski <kkowalski0477@gmail.com>
2024-08-29 11:37:22 +0200

    Play button
    A button responsible for starting a song

---
+++ Audio_player(GUI)/PlayButton.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class PlayButton extends JButton {
+
+    PlayButton(){
+
+        this.setText("PLAY");
+        this.setForeground(Color.GREEN);
+        this.setBackground(Color.BLACK);
+        this.setBorderPainted(false);
+
+    }
+}
