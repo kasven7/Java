ebbffbbd8075f05ef28c02fa05f6f3d8e22cff9f
Kacper Kowalski <kkowalski0477@gmail.com>
2024-08-29 11:38:19 +0200

    Stop button
    The button responsible for stopping the song

---
+++ Audio_player(GUI)/StopButton.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class StopButton extends JButton {
+
+    StopButton() {
+
+        this.setText("STOP");
+        this.setForeground(Color.GREEN);
+        this.setBackground(Color.BLACK);
+        this.setBorderPainted(false);
+    }
+}
