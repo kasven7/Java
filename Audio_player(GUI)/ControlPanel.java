a9914e50e1ae5809aa215b045c9dfbe1078fe3a3
Kacper Kowalski <kkowalski0477@gmail.com>
2024-09-02 10:29:09 +0200

    An updated version of the program
    Every button in the control panel works as intended, three seperate classes for stop, play and reset buttons have been replaced by one class

--- Audio_player(GUI)/ControlPanel.java
+++ Audio_player(GUI)/ControlPanel.java
@@ -1,23 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
 
 
 public class ControlPanel extends JPanel {
 
-    ControlPanel() {
+    ControlPanel() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
 
-        PlayButton play = new PlayButton();
-        StopButton stop = new StopButton();
-        ResetButton reset = new ResetButton();
+        // File clip and audio input stream
+        File file = new File("antent - hope to see you again (slowed).wav");
+        AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(ais);
+
+        // Control panel buttons
+        KButton play = new KButton();
+        play.setText("PLAY");
+
+        KButton stop = new KButton();
+        stop.setText("STOP");
+
+        KButton reset = new KButton();
+        reset.setText("RESET");
+
+        // Panel properties
         this.setBackground(Color.black);
         this.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        this.setBounds(300, 300, 250, 37);
 
+        // button functionality
+        play.addActionListener(_ -> clip.start());
+        stop.addActionListener(_ -> clip.stop());
+        reset.addActionListener(_ -> clip.setFramePosition(0));
 
         this.add(play);
         this.add(stop);
         this.add(reset);
-        this.setBounds(300, 300, 250, 37);
-
 
     }
 }
