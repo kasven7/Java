f77d978c2400e3cd94147c2d62267293e90d01c2
Kacper Kowalski <kkowalski0477@gmail.com>
2024-10-05 15:29:37 +0200

    Update
    Now isPrime function checks the i value equal to that of an square root of number

--- College/Lab2/lab2_p/zad1.java
+++ College/Lab2/lab2_p/zad1.java
@@ -11,7 +11,7 @@ public class zad1 {
         }
 
 
-        for(int i = 2; i < Math.sqrt(number); i++){
+        for(int i = 2; i <= Math.sqrt(number); i++){
             if(number % i == 0){
                 return false;
 
