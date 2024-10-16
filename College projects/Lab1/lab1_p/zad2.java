f6e46c33c30374335a06fe152becd58dd7bfab1a
Kacper Kowalski <kkowalski0477@gmail.com>
2024-10-16 10:28:15 +0200

    Add files via upload

--- College projects/Lab1/lab1_p/zad2.java
+++ College projects/Lab1/lab1_p/zad2.java
@@ -1,30 +1,29 @@
-package lab1_p;
+package lab2_p;
+import java.lang.Math;
 import java.util.Scanner;
 
 
-class zad2{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+public class zad2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
 
-        System.out.println("Enter a value of parameter \"a\" of your square equation: ");
-        double a = sc.nextDouble();
+        System.out.print("a = ");
+        double a = input.nextDouble();
 
-        System.out.println("Enter a value of parameter \"b\" of your square equation: ");
-        double b = sc.nextDouble();
+        System.out.print("b = ");
+        double b = input.nextDouble();
 
-        System.out.println("Enter a value of parameter \"c\" of your square equation: ");
-        double c = sc.nextDouble();
-
-        double delta = Math.pow(b, 2) - 4 * a * c;
+        System.out.print("c = ");
+        double c = input.nextDouble();
 
 
+        double delta = Math.pow(b, 2) - (4 * a * c);
         System.out.println("Here is your square equation: " + a + "x^2 + " + b + "x + " + c + " = 0");
         System.out.println("Δ = " + delta);
 
+
         double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-
-
         if(delta > 0){
             double x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
@@ -40,8 +39,9 @@ class zad2{
 
         }
 
-        else{
+        else if (delta < 0){
             System.out.println("Your square equation doesn't have a solution in real numbers!");
         }
+        input.close();
     }
 }
