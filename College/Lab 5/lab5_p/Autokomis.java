3a1f403a3aa6625f91fb4409d53bb2ff28c10087
Kacper Kowalski <kkowalski0477@gmail.com>
2024-12-11 09:53:26 +0100

    Add files via upload

--- College/Lab 5/lab5_p/Autokomis.java
+++ College/Lab 5/lab5_p/Autokomis.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 
 public class Autokomis {
-    private final ArrayList<Samochod> komis = new ArrayList<>();
+    private ArrayList<Samochod> komis = new ArrayList<>();
 
 
     public Autokomis(){
