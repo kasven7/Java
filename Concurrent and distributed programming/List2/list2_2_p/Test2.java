f880814248a299fd065c76157bd2d6f86e74552c
Kacper Kowalski <kkowalski0477@gmail.com>
2025-03-14 11:17:58 +0100

    Add files via upload

---
+++ Concurrent and distributed programming/List2/list2_2_p/Test2.java
@@ -0,0 +1,16 @@
+package list2_2;
+
+public class Test2 {
+    public static void main(String[] args){
+        W1 w1 = new W1();
+        W2 w2 = new W2();
+
+        Thread t1 = new Thread(w1);
+        Thread t2 = new Thread(w2);
+
+        t1.start();
+        t2.start();
+
+        System.out.println("KONIEC PROGRAMU");
+    }
+}
---
+++ Concurrent and distributed programming/List2/list2_2_p/Watek2.java
@@ -0,0 +1,11 @@
+package list2_2;
+
+
+public class Watek2 extends Thread{
+    @Override
+    public void run(){
+        for(int i = 1; i <= 20; i++){
+            System.out.println(Thread.currentThread().getName() + " | i = " + i);
+        }
+    }
+}
