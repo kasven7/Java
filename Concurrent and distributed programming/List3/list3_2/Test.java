c583e39f73169eb20fcbb45c6346da29ea858256
Kacper Kowalski <kkowalski0477@gmail.com>
2025-03-21 11:25:07 +0100

    Second half of the list

---
+++ Concurrent and distributed programming/List3/list3_2/Test.java
@@ -0,0 +1,29 @@
+package lab3_2;
+
+
+public class Test {
+    public static void main(String[] args){
+        int sleepValue = (int) (Math.random() % 100) + 1;
+        Watek[] watek = new Watek[5];
+
+
+
+        for(int i = 4; i >= 0; i--){
+            watek[i] = new Watek();
+            watek[i].setName("W" + (i + 1));
+
+            try {
+                Thread.sleep(sleepValue);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            watek[i].start();
+
+            try {
+                watek[i].join(); // ensures that the current thread finishes its tasks before starting the next
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }}
---
+++ Concurrent and distributed programming/List3/list3_2/Watek.java
@@ -0,0 +1,10 @@
+package lab3_2;
+
+
+public class Watek extends Thread{
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            System.out.println("Hello "+ Thread.currentThread().getName() + "!");
+        }
+    }
+}
